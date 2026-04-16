c79c3ab5d3b585d1485a329012dde8a50cf614e8
crystaltech <crystaltech@users.noreply.github.com>
2026-04-16 08:07:07 -0500

    release: v1.8.9

--- GoldAdvisorMidnight/Constants.lua
+++ GoldAdvisorMidnight/Constants.lua
@@ -15,7 +15,7 @@ local function ProfileDefault(profileKey, field, fallback)
 end
 
 GAM.C = {
-    ADDON_VERSION        = "1.8.8",
+    ADDON_VERSION        = "1.8.9",
     DATA_VERSION         = 13,
     DEFAULT_PATCH        = "midnight-1",
 
