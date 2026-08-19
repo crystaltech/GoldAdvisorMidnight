d10201cd5385c701d12c57dc25e1361547c99e3d
crystaltech <techbound@gmail.com>
2026-08-19 09:32:48 -0500

    Update Constants.lua

--- GoldAdvisorMidnight/Constants.lua
+++ GoldAdvisorMidnight/Constants.lua
@@ -36,7 +36,7 @@ for _, profession in ipairs(PROFESSION_REGISTRY) do
 end
 
 GAM.C = {
-    ADDON_VERSION        = "2.0.2",
+    ADDON_VERSION        = "2.0.4",
     DATA_VERSION         = 18,
     STRATEGY_SCHEMA_VERSION = 1,
     DEFAULT_PATCH        = "midnight-1",
